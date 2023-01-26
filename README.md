7444e14ccb96f1dc6dbe91df90c5c54585689d61
Deep Mehta <deepme987@gmail.com>
2023-01-26 17:26:17 -0500

    updated required functions

--- README.md
+++ README.md
@@ -1,4 +1,43 @@
 # WasItRufus
-Assessment for Was It Rufus
+Assessment for WasItRufus?
 
-RandomLocalChange
+Here, I'm using one of my existing repo for test purpose: https://github.com/deepme987/Rep2Rec
+
+Installation: `pip install GitPython`
+
+To initialize (main): `rufus = BlameRufus(git_dir_name)`
+
+Sample output (initial commits are from a week ago)
+```angular2html
+active branch: master
+local changes: False
+recent commit: False
+blame Rufus: False
+```
+
+After making changes (no commit yet):
+
+```angular2html
+active branch: master
+local changes: True
+recent commit: False
+blame Rufus: False
+```
+
+After committing the change, note: author tested was `"Deep"` here, not `"Rufus"`:
+
+```angular2html
+active branch: master
+local changes: False
+recent commit: True
+blame Rufus: True
+```
+
+After changing the branch:
+
+```angular2html
+active branch: test
+local changes: False
+recent commit: True
+blame Rufus: True
+```
--- wasitrufus.py
+++ wasitrufus.py
@@ -1,17 +1,65 @@
 
-class BlameRufus:
-    def __init__(self, git_dir):
-        self._git_dir = git_dir
+import os
+import git
+import datetime
 
-    def active_branch(self):
+REPOSITORY_DIR = "Rep2Rec"
+
+
+class BlameRufus:
+    def __init__(self, git_dir: str = "") -> None:
+        self._git_dir = os.getcwd() + "/" + git_dir
+        self.Repo = None
+        assert self.is_git_repo()
+
+    def is_git_repo(self) -> bool:
+        """ Tests whether the given directory is a valid directory/ repository """
+        try:
+            self.Repo = git.Repo(self._git_dir)
+            return True
+        except git.exc.InvalidGitRepositoryError:
+            print("Not a valid Git Repository")
+            return False
+        except git.exc.NoSuchPathError:
+            print("Not a valid Path")
+            return False
+
+    def active_branch(self) -> str:
+        """ Fetch the current branch from the repository """
+
+        """
+        # Archived code to manually get the branch
         head_dir = self._git_dir + "/.git/HEAD"
-        with head_dir.open("r") as f:
+        with open(head_dir, "r") as f:
             content = f.read().splitlines()
 
         for line in content:
             if line[0:4] == "ref:":
                 return line.partition("refs/heads/")[2]
+        """
 
-    def local_changes(self):
+        return self.Repo.active_branch
+
+    def local_changes(self) -> bool:
+        """" Returns a bool for any untracked changes in local repository """
+        return self.Repo.is_dirty(untracked_files=True)
+
+    def last_commit(self) -> bool:
+        """ Returns a bool to indicate whether the last local commit was made within a week """
+        week_ago = (datetime.datetime.now() - datetime.timedelta(days=7)).timestamp()
+        commit_date = self.Repo.commit().committed_date
+        return week_ago < commit_date
+
+    def was_it_rufus(self, name: str = "Rufus") -> bool:
+        """ It was not me, IT WAS RUFUS! """
+        commit = self.Repo.commit()
+        # Checks for name as well as email
+        return any([name.lower() in author.lower() for author in [commit.author.name, commit.author.email]])
+
 
 if __name__ == '__main__':
+    rufus = BlameRufus(REPOSITORY_DIR)
+    print(f"active branch: {rufus.active_branch()}")
+    print(f"local changes: {rufus.local_changes()}")
+    print(f"recent commit: {rufus.last_commit()}")
+    print(f"blame Rufus: {rufus.was_it_rufus()}")
